1cd2e0501c43a7d5813278ee3b368ac97fd3c612
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-17 11:12:44 -0700

    add handleInputChange function

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,7 +3,6 @@ import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client"; // Import useMutation
 import { ADD_USER } from "../utils/mutations"; // Import the ADD_USER mutation
 
-import { createUser } from "../utils/API";
 import Auth from "../utils/auth";
 
 const SignupForm = () => {
@@ -64,7 +63,50 @@ const SignupForm = () => {
         >
           Something went wrong with your signup!
         </Alert>
-        {/* Rest of the form input fields */}
+        <Form.Group>
+          <Form.Label htmlFor="username">Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your username"
+            name="username"
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Username is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label htmlFor="email">Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Your email address"
+            name="email"
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Email is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your password"
+            name="password"
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
+        </Form.Group>
         <Button
           disabled={
             !(
