24f529126dbd5732a1adb04341b3a850600a29ee
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-18 09:27:38 -0700

    starting over by adding files from unsolved folder, reason app not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,30 @@
-import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SearchBooks from "./pages/SearchBooks";
-import SavedBooks from "./pages/SavedBooks";
-import Navbar from "./components/Navbar";
-
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  uri: "/graphql",
-  cache: new InMemoryCache(),
-});
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
+    <Router>
+      <>
         <Navbar />
         <Routes>
-          <Route path="/" element={<SearchBooks />} />
-          <Route path="/saved" element={<SavedBooks />} />
-          <Route
-            path="*"
-            element={<h1 className="display-2">Wrong page!</h1>}
+          <Route 
+            path='/' 
+            element={<SearchBooks />} 
+          />
+          <Route 
+            path='/saved' 
+            element={<SavedBooks />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
           />
         </Routes>
-      </Router>
-    </ApolloProvider>
+      </>
+    </Router>
   );
 }
 
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-import { Form, Button, Alert } from "react-bootstrap";
-import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../utils/mutations";
-import Auth from "../utils/auth";
+// see SignupForm.js for comments
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+
+import { loginUser } from '../utils/API';
+import Auth from '../utils/auth';
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
-  const [loginUser, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -19,6 +18,7 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
+    // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -26,63 +26,62 @@ const LoginForm = () => {
     }
 
     try {
-      const { data } = await loginUser({ variables: { ...userFormData } });
-      Auth.login(data.login.token);
+      const response = await loginUser(userFormData);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
 
     setUserFormData({
-      email: "",
-      password: "",
+      username: '',
+      email: '',
+      password: '',
     });
   };
 
   return (
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert
-          dismissible
-          onClose={() => setShowAlert(false)}
-          show={showAlert || error} // Show alert if error is truthy
-          variant="danger"
-        >
-          {error ? "Something went wrong with your login credentials!" : null}
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your login credentials!
         </Alert>
         <Form.Group>
-          <Form.Label>Email</Form.Label>
+          <Form.Label htmlFor='email'>Email</Form.Label>
           <Form.Control
-            type="email"
-            placeholder="Your email"
-            name="email"
+            type='text'
+            placeholder='Your email'
+            name='email'
+            onChange={handleInputChange}
             value={userFormData.email}
-            onChange={handleInputChange}
             required
           />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid email.
-          </Form.Control.Feedback>
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
         </Form.Group>
+
         <Form.Group>
-          <Form.Label>Password</Form.Label>
+          <Form.Label htmlFor='password'>Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Your password"
-            name="password"
-            value={userFormData.password}
+            type='password'
+            placeholder='Your password'
+            name='password'
             onChange={handleInputChange}
+            value={userFormData.password}
             required
           />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid password.
-          </Form.Control.Feedback>
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
         </Form.Group>
         <Button
           disabled={!(userFormData.email && userFormData.password)}
-          type="submit"
-          variant="success"
-        >
+          type='submit'
+          variant='success'>
           Submit
         </Button>
       </Form>
