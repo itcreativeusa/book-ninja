78b870cc0f73459333373411cd8e7ac156485cb8
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-18 09:24:06 -0700

    auth js change module export

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,45 +1,40 @@
 const jwt = require("jsonwebtoken");
 
-// Set token secret and expiration date
+// set token secret and expiration date
 const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
-// Function for our authenticated routes
-const authMiddleware = function (req, res, next) {
-  // Allows token to be sent via req.query or headers
-  let token = req.query.token || req.headers.authorization;
-
-  // ["Bearer", "<tokenvalue>"]
-  if (req.headers.authorization) {
-    token = token.split(" ").pop().trim();
-  }
-
-  if (!token) {
-    return res.status(400).json({ message: "You have no token!" });
-  }
-
-  // Verify token and get user data out of it
-  try {
-    const { data } = jwt.verify(token, secret, { maxAge: expiration });
-    req.user = data;
-  } catch {
-    console.log("Invalid token");
-    return res.status(400).json({ message: "Invalid token!" });
-  }
-
-  // Send to next endpoint
-  next();
-};
-
-// Function to sign a token with provided user data
-const signToken = function ({ username, email, _id }) {
-  const payload = { username, email, _id };
-
-  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-};
-
-// Export the functions
 module.exports = {
-  authMiddleware,
-  signToken,
+  authMiddleware: function ({ req, res }, next) {
+    // allows token to be sent via headers
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ message: "Authorization token missing or invalid." });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Authorization token missing or invalid." });
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret);
+      req.user = data;
+      next();
+    } catch (error) {
+      console.log("Invalid token");
+      return res.status(401).json({ message: "Invalid token." });
+    }
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };
