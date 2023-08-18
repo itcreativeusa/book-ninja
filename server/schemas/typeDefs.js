3faf15e3ff26369f7c3f483a53533a45818ac7aa
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-18 10:23:16 -0700

    add typedefs js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,21 +1,56 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
+  input UserInput {
+    username: String
+    email: String
+    password: String
+  }
 
+  input BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
   }
 
   type Book {
-
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
   }
 
   type Query {
     me: User
+    users: [User]
+    user(username: String!): User
+    userById(_id: ID!): User
+    books: [Book]
   }
 
   type Mutation {
-    createUser(body: User!): User
-    
+    createUser(body: UserInput!): User
+    login(email: String!, password: String!): User
+    saveBook(body: BookInput!): User
+    deleteBook(bookId: String!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
